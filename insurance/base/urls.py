2ebc0df7fc9e7a5f1910f3bb5decbeb1134753d4
namank03 <khandelwalnaman03@gmail.com>
2021-10-30 19:53:47 +0530

    added update policy functionality

--- insurance/base/urls.py
+++ insurance/base/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from insurance.base.views import create_policy, deletePolicy, policy_view
+from insurance.base.views import create_policy, deletePolicy, policy_view, update_policy
 
 app_name = "base"
 
 urlpatterns = [
     path("", view=policy_view, name="policies"),
     path('create-policy/', create_policy, name="createPolicy"),
+    path('create-policy/<str:pk>', update_policy, name="updatePolicy"),
     path('delete-policy/<str:pk>', deletePolicy, name="deletePolicy"),
 ]
--- insurance/base/views.py
+++ insurance/base/views.py
@@ -40,3 +40,19 @@ def deletePolicy(request, pk):
         policy.delete()
         return redirect('base:policies')
     return render(request, 'pages/delete.html', {'obj': policy})
+
+
+@login_required(login_url='login')
+def update_policy(request, pk):
+    policy = Policy.objects.get(id=pk)
+    # Prefilling the form with the policy instance value
+    form = PolicyForm(instance=policy)
+    if request.method == 'POST':
+        policy.fuel = request.POST.get('fuel')
+        policy.vehicle_segment = request.POST.get('vehicle_segment')
+        policy.premium = request.POST.get('premium')
+        policy.customer = Customer.objects.get(id=request.POST.get('customer'))
+        policy.save()
+        return redirect('base:policies')
+    context = {'form': form, 'policy': policy}
+    return render(request, 'policy/policy_form.html', context)
