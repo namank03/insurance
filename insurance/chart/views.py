e8bc14243147890390a2e378506a251633040177
namank03 <khandelwalnaman03@gmail.com>
2021-10-31 14:20:29 +0530

    updated policy table

--- insurance/chart/views.py
+++ insurance/chart/views.py
@@ -5,13 +5,15 @@ from django.db.models.functions import ExtractMonth, ExtractYear
 from django.http import JsonResponse
 from django.shortcuts import render
 
-from insurance.base.models import Customer, Policy
+from insurance.base.models import Policy
 from insurance.utils.charts import colorPrimary, get_year_dict, months
 
 
 def get_filter_options(request):
+    # q = request.GET.get("q") if request.GET.get("q") is not None else ""
+
     grouped_policies = (
-        Policy.objects.annotate(year=ExtractYear("created_at"))
+        Policy.objects.annotate(year=ExtractYear("date_of_purchase"))
         .values("year")
         .order_by("-year")
         .distinct()
@@ -26,10 +28,12 @@ def get_filter_options(request):
 
 
 def get_policy_chart(request, year, region):
-    policies = Policy.objects.filter(created_at__year=year, customer__region=region)
+    policies = Policy.objects.filter(
+        date_of_purchase__year=year, customer__region=region
+    )
     grouped_policies = (
         policies.annotate(price=F("premium"))
-        .annotate(month=ExtractMonth("created_at"))
+        .annotate(month=ExtractMonth("date_of_purchase"))
         .values("month")
         .annotate(average=Count("premium"))
         .values("month", "average")
@@ -43,7 +47,7 @@ def get_policy_chart(request, year, region):
 
     return JsonResponse(
         {
-            "title": f"Policies {year}",
+            "title": f"Policies {year} -  Region {region.upper()}",
             "data": {
                 "labels": list(policy_dict.keys()),
                 "datasets": [
